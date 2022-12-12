41f28b16e5e01b7083c63c79467054f3aa97fb6f
Kacper Duda <120419596+FreikaE@users.noreply.github.com>
2022-12-12 22:34:32 +0100

    La in loudness kod

--- Loudness.R
+++ Loudness.R
@@ -1,18 +1,28 @@
-#Ladda in paket
+#Load packages
 library(readr)
 library(tidyverse)
 library(MASS)
 
-#500 Hz - VÄNSTER
 #Read in data
 longdata <- read_delim("longdata.csv", delim = ";", 
                        escape_double = FALSE, trim_ws = TRUE)
 
+dat5L <- longdata[c("0.5kHz-upplevd-L", "0.5kHz-dB-L")]
+names(dat5L) <- c("loudness", "dB")
+dat5R <- longdata[c("0.5kHz-upplevd-R", "0.5kHz-dB-R")]
+names(dat5R) <- c("loudness", "dB")
+dat4L <- longdata[c("4kHz-upplevd-L", "4kHz-dB-L")]
+names(dat4L) <- c("loudness", "dB")
+dat4R <- longdata[c("4kHz-upplevd-R", "4kHz-dB-R")]
+names(dat4R) <- c("loudness", "dB")
+
 #Create linear model
 mod5L <- lm(longdata$`0.5kHz-upplevd-L` ~ longdata$`0.5kHz-dB-L`) 
+mod5R <- lm(loudness ~ dB, data = dat5R)
+
 mod5R <- lm(longdata$`0.5kHz-upplevd-R` ~ longdata$`0.5kHz-dB-R`) 
-mod4L <- lm(longdata$`4kHz-upplevd - L` ~ longdata$`4kHz-dB-L`) 
-mod4R <- lm(longdata$`4kHz-upplevd - R` ~ longdata$`4kHz-dB-R`) 
+mod4L <- lm(longdata$`4kHz-upplevd-L` ~ longdata$`4kHz-dB-L`) 
+mod4R <- lm(longdata$`4kHz-upplevd-R` ~ longdata$`4kHz-dB-R`) 
 
 #Print summary of linear model
 summary(mod5L)
@@ -27,24 +37,50 @@ plot(longdata$`0.5kHz-dB-L`, longdata$`0.5kHz-upplevd-L`,
 plot(longdata$`0.5kHz-dB-R`, longdata$`0.5kHz-upplevd-R`,
      xlab="dB Level",
      ylab="Loudness")
-plot(longdata$`4kHz-dB-L`, longdata$`4kHz-upplevd - L`,
+plot(longdata$`4kHz-dB-L`, longdata$`4kHz-upplevd-L`,
      xlab="dB Level",
      ylab="Loudness")
-plot(longdata$`4kHz-dB-L`, longdata$`4kHz-upplevd - L`,
+plot(longdata$`4kHz-dB-L`, longdata$`4kHz-upplevd-L`,
      xlab="dB Level",
      ylab="Loudness")
 
 #Inspect class of "upplevd" variable
 class(longdata$`0.5kHz-upplevd-L`)
+class(longdata$`0.5kHz-upplevd-R`)
+class(longdata$`4kHz-upplevd-L`)
+class(longdata$`4kHz-upplevd-R`)
 
 #Overwrite upplevd as factor variable
 longdata$`0.5kHz-upplevd-L` <- factor(longdata$`0.5kHz-upplevd-L`)
+longdata$`0.5kHz-upplevd-R` <- factor(longdata$`0.5kHz-upplevd-R`)
+longdata$`4kHz-upplevd-L` <- factor(longdata$`4kHz-upplevd-L`)
+longdata$`4kHz-upplevd-R` <- factor(longdata$`4kHz-upplevd-R`)
 
 #Create logistics model
 mod5L_glm <- glm(longdata$`0.5kHz-upplevd-L` ~ longdata$`0.5kHz-dB-L`, family = "binomial")
+mod5R_glm <- glm(longdata$`0.5kHz-upplevd-L` ~ longdata$`0.5kHz-dB-L`, family = "binomial")
+mod4L_glm <- glm(longdata$`4kHz-upplevd-L` ~ longdata$`4kHz-dB-L`, family = "binomial")
+mod4R_glm <- glm(longdata$`4kHz-upplevd-R` ~ longdata$`4kHz-dB-R`, family = "binomial")
 
 #Inspect logistic model
 summary(mod5L_glm)
 summary(mod5R_glm)
 summary(mod4L_glm)
 summary(mod4R_glm)
+
+#A nicer plot(?) option using ggplot-package
+plot1 <- ggplot(dat5, aes(x = dB_presentation, y = loudness_score)) + 
+  geom_point(shape = 1, size = 3) +
+  stat_smooth(method = "lm", col = "red", alpha = 0.35)+
+  xlab("Level presented (dB HL)")+
+  ylab("Loudness category")+
+  theme_bw()
+
+plot2 <- ggplot(longdata, aes(x = longdata$`0.5kHz-dB-L`, y = longdata$`0.5kHz-upplevd-L`)) + 
+  geom_point(shape = 1, size = 3) +
+  stat_smooth(method = "lm", col = "red", alpha = 0.35)+
+  xlab("Level presented (dB HL)")+
+  ylab("Loudness category")+
+  theme_bw()
+
+ggarrange(plot2, plot2, ncol=2, nrow=1)
