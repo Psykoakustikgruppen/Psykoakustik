24540d4dcc5ea098b52a849cd3b68c524b5d1808
Kacper Duda <120419596+FreikaE@users.noreply.github.com>
2022-12-13 12:50:10 +0100

    Uppdaterad kod
    NYTT:
    - Linjära grafer finns, grupperade efter frekvens
    - Logistiska grafer finns, grupperade efter frekvens
    
    FEL:
    - Logistisk linje hamnar fel i graferna, vet ej varför.

--- Loudness.R
+++ Loudness.R
@@ -16,43 +16,39 @@ library(ggpubr)
 longdata <- read_delim("longdata.csv", delim = ";", 
                        escape_double = FALSE, trim_ws = TRUE)
 
-#Rename
-dat5L <- longdata[c("0.5kHz-upplevd-L", "0.5kHz-dB-L")]
-names(dat5L) <- c("loudness", "dB")
-dat5R <- longdata[c("0.5kHz-upplevd-R", "0.5kHz-dB-R")]
-names(dat5R) <- c("loudness", "dB")
-dat4L <- longdata[c("4kHz-upplevd-L", "4kHz-dB-L")]
-names(dat4L) <- c("loudness", "dB")
-dat4R <- longdata[c("4kHz-upplevd-R", "4kHz-dB-R")]
-names(dat4R) <- c("loudness", "dB")
-
+#LINJÄRA MODELLER
 #Create linear model
 mod5L <- lm(longdata$`0.5kHz-upplevd-L` ~ longdata$`0.5kHz-dB-L`) 
-mod5R <- lm(loudness ~ dB, data = dat5R)
-
 mod5R <- lm(longdata$`0.5kHz-upplevd-R` ~ longdata$`0.5kHz-dB-R`) 
 mod4L <- lm(longdata$`4kHz-upplevd-L` ~ longdata$`4kHz-dB-L`) 
 mod4R <- lm(longdata$`4kHz-upplevd-R` ~ longdata$`4kHz-dB-R`) 
 
-#Print summary of linear model
-summary(mod5L)
-summary(mod5R)
-summary(mod4L)
-summary(mod4R)
-
-#Plot data from linear model
-plot(longdata$`0.5kHz-dB-L`, longdata$`0.5kHz-upplevd-L`,
-     xlab="dB Level",
-     ylab="Loudness")
+#Linjära grafer - 500 Hz
+par(mfrow=c(1,2))
 plot(longdata$`0.5kHz-dB-R`, longdata$`0.5kHz-upplevd-R`,
-     xlab="dB Level",
-     ylab="Loudness")
+     xlab="Level presented (dB HL)",
+     ylab="Loudness (numeric)")
+abline(mod5R)
+
+plot(longdata$`0.5kHz-dB-L`, longdata$`0.5kHz-upplevd-L`,
+     xlab="Level presented (dB HL)",
+     ylab="Loudness (numeric)")
+abline(mod5L)
+
+#Linjära grafer - 4000 Hz
+par(mfrow=c(1,2))
+plot(longdata$`4kHz-dB-R`, longdata$`4kHz-upplevd-R`,
+     xlab="Level presented (dB HL)",
+     ylab="Loudness (numeric)")
+abline(mod4R)
+
 plot(longdata$`4kHz-dB-L`, longdata$`4kHz-upplevd-L`,
-     xlab="dB Level",
-     ylab="Loudness")
-plot(longdata$`4kHz-dB-L`, longdata$`4kHz-upplevd-L`,
-     xlab="dB Level",
-     ylab="Loudness")
+     xlab="Level presented (dB HL)",
+     ylab="Loudness (numeric)")
+abline(mod4L)
+
+
+#LOGISTISKA MODELLER
 
 #Inspect class of "upplevd" variable
 class(longdata$`0.5kHz-upplevd-L`)
@@ -78,19 +74,38 @@ summary(mod5R_glm)
 summary(mod4L_glm)
 summary(mod4R_glm)
 
-#A nicer plot(?) option using ggplot-package
-plot1 <- ggplot(dat5, aes(x = dB_presentation, y = loudness_score)) + 
+# Grafer för 500 Hz - höger & vänster öra
+plot1 <- ggplot(longdata, aes(x = longdata$`0.5kHz-dB-R`, y = longdata$`0.5kHz-upplevd-R`)) + 
   geom_point(shape = 1, size = 3) +
   stat_smooth(method = "lm", col = "red", alpha = 0.35)+
   xlab("Level presented (dB HL)")+
-  ylab("Loudness category")+
+  ylab("Loudness (category)")+
   theme_bw()
 
 plot2 <- ggplot(longdata, aes(x = longdata$`0.5kHz-dB-L`, y = longdata$`0.5kHz-upplevd-L`)) + 
   geom_point(shape = 1, size = 3) +
-  stat_smooth(method = "lm", col = "red", alpha = 0.35)+
+  stat_smooth(method = "lm", col = "blue", alpha = 0.35)+
   xlab("Level presented (dB HL)")+
-  ylab("Loudness category")+
+  ylab("Loudness (category)")+
   theme_bw()
 
-ggarrange(plot2, plot2, ncol=2, nrow=1)
+# 500 HZ : Lägg graferna för höger (plot1) och vänster (plot2) öra bredvid varandra
+ggarrange(plot1, plot2, ncol=2, nrow=1)
+
+# Grafer för 4 kHz - höger & vänster öra
+plot3 <- ggplot(longdata, aes(x = longdata$`4kHz-dB-R`, y = longdata$`4kHz-upplevd-R`)) + 
+  geom_point(shape = 1, size = 3) +
+  stat_smooth(method = "lm", col = "red", alpha = 0.35)+
+  xlab("Level presented (dB HL)")+
+  ylab("Loudness (category)")+
+  theme_bw()
+
+plot4 <- ggplot(longdata, aes(x = longdata$`4kHz-dB-L`, y = longdata$`4kHz-upplevd-L`)) + 
+  geom_point(shape = 1, size = 3) +
+  stat_smooth(method = "lm", col = "blue", alpha = 0.35)+
+  xlab("Level presented (dB HL)")+
+  ylab("Loudness (category)")
+  theme_bw()
+
+# 4 kHz: Lägg graferna för höger (plot3) och vänster (plot4) öra - 4kHz, bredvid varandra
+ggarrange(plot3, plot4, ncol=2, nrow=1)
